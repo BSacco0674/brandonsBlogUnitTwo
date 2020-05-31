32a9e832aee943cdeaa92970055ecf5ee40225de
Bsacco9814 <Brandon.Sacco13@gmail.com>
2020-05-31 09:45:16 -0500

    1stcommitomg

--- config/database.js
+++ config/database.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose');
+const db = mongoose.connection;
+
 
 mongoose.connect('mongodb://localhost/tktks', {
   useNewUrlParser: true, 
   useCreateIndex: true, 
   useUnifiedTopology: true
+});
+
+db.on('connected', function() {
+  console.log(`Mongoose connected to ${db.host}: ${db.port}`)
 });
--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-const session = require('express-session');
-const passport = require('passport');
+const session = require("express-session");
+const passport = require("passport");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const indexRouter = require("./routes/index");
 const tktksRouter = require("./routes/tktks");
-const usersRouter = require('./routes/users');
+const usersRouter = require("./routes/users");
 
 require("./config/database");
 
@@ -24,11 +24,13 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({
-  secret: 'MySecret',
-  resave: false,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: "MySecret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(passport.initialize());
@@ -36,15 +38,15 @@ app.use(passport.session());
 
 app.use("/", indexRouter);
 app.use("/tktks", tktksRouter);
-app.use('/users', usersRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
